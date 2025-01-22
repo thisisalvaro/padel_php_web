f6596c33dff7e537db3638d386a9f6109a3d3794
joa <joaquin_trujillo@icloud.com>
2025-01-23 00:27:01 +0100

    build: login ui

--- public/index.php
+++ public/index.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Padel</title>
-    <link rel="stylesheet" href="css/index.css">
+    <title>Padel Page</title>
+    <link rel="stylesheet" href="/public/css/index.css">
+    <link rel="icon" href="/public/images/raqueta-de-padel.png">
+    <style>
+        .button {
+            background-color: var(--yellow);
+            color: var(--black);
+            border-radius: 10px;
+            padding: 8px 16px;
+        }
+    </style>
 </head>
 <body>
-    
+    <h1>Index page</h1>
+    <button class="button">Click here</button>
 </body>
 </html>
---
+++ views/auth/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Padel Page - Login</title>
+    <link rel="stylesheet" href="/public/css/auth.css">
+    <link rel="icon" href="/public/images/raqueta-de-padel.png">
+</head>
+<body class="form-body">
+    <main class="main-container">
+        <section class="form-container">
+            <h1>Login</h1>
+            <span>Bienvenido de nuevo</span>
+            <form class="login-form" action="" method="post">
+                <label for="email">
+                    Email
+                    <input type="email" name="email" id="email" placeholder="Correo electrónico">
+                </label>
+
+                <label for="password">
+                    Password
+                    <input type="password" name="password" id="password" placeholder="****">
+                </label>
+
+                <input type="submit" value="Enviar">
+            </form>
+            <a href="#">Si aún no estás registrado, regístrate</a>
+        </section>
+    </main>
+</body>
+</html>
