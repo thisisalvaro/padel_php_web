5551385a91ce9c99b4ec850371c09fe991580285
joa <joaquin_trujillo@icloud.com>
2025-01-29 20:08:55 +0100

    fix: db connection

--- config/db.php
+++ config/db.php
@@ -1,7 +1,8 @@
 <?php
-$host = 'localhost';
-$db = 'padel';
-$user = 'root';
-$pass = '';
+$connectionString = "postgresql://neondb_owner:npg_ORn5JSxC2Bur@ep-sweet-mountain-a8qbzzx0.eastus2.azure.neon.tech/neondb?sslmode=require&options=endpoint%3Dep-sweet-mountain-a8qbzzx0";
 
-$db = mysqli_connect($host, $user, $pass, $db);
+$db = pg_connect($connectionString);
+
+if (!$db) {
+    die("Error al conectar a la base de datos.");
+} 
