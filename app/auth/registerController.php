842543d9485d4b77c39d828a0b641b6c5807cd88
joa <joaquin_trujillo@icloud.com>
2025-02-05 17:09:32 +0100

    up

--- app/auth/registerController.php
+++ app/auth/registerController.php
@@ -1,2 +1,28 @@
 <?php
 // controlador para manejar la lógica del registro
+class RegisterController {
+    public function index() {
+        renderView('auth/register');
+    }
+
+    public function register() {
+        // require_once '../../config/db.php';
+
+        if (is_post()) {
+            $name = $_POST['name'] ?? '';
+            $lastname = $_POST['lastname'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $password = $_POST['password'] ?? '';
+
+            Database::register($name, $lastname, $email, $password);
+
+            // make the logic of the registration in this method
+            
+        } else {
+            $this->index();
+        }
+    }
+}
+
+$registerController = new RegisterController();
+$registerController->register();
