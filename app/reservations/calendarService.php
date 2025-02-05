c49212be7b80eb6e134c6c3184f3da408ffb1341
thisisalvaro <alvaroblanco496@gmail.com>
2025-02-05 17:33:58 +0100

    (fix) There wasnt any class for the database method

--- app/reservations/calendarService.php
+++ app/reservations/calendarService.php
@@ -5,13 +5,15 @@ require_once 'db.php'; // Archivo de conexión a la base de datos
 
 // Obtener todos los horarios disponibgit pullles para una fecha y cancha específica
 function obtenerHorariosDisponibles($fecha, $id_pista) {
+    $reservationcontroler = new ReservationController();
+
     $horarios = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
     
     if ($fecha == null || $id_pista == null) {
         return [];
     }
     
-    $reservas = obtenerReservasPorFechaYPista($fecha, $id_pista);
+    $reservas = $reservationcontroler->obtenerReservasPorFechaYPista($fecha, $id_pista);
     
     // Filtra los horarios ocupados
     return array_diff($horarios, $reservas);
