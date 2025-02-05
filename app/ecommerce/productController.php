010d1fe2b619edf38baa1a7aa466679dc2fdfd5f
Jorge <jorgevivar2005@gmail.com>
2025-02-05 17:40:47 +0100

    update commit

--- app/ecommerce/productController.php
+++ app/ecommerce/productController.php
@@ -3,12 +3,23 @@ require_once '../../config/config.php';
 
 class ListProductController {
 
-    &private $connection;
+    private $connection;
+
+    public function __construct( $connection) {
+        $this->connection = $connection;
+    }
+
+    public function listProduct() {
+        $response = pg_query($this->connection, "SELECT * FROM productos");
     
-    $this->connection = Database::getConnection();
-
-
-    
-
+        while ($row = pg_fetch_row($response)) {
+            echo "id: $row[0]  nombre: $row[1] descripcion: $row[2] precio: $row[3] categoria: $row[4] marca: $row[5] stock: $row[6]";
+            echo "<br />\n";
+        }
+    }
 
 }
+
+
+$controller = new ListProductController( Database::getConnection());
+$controller->listProduct();
