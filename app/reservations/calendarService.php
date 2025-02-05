ed386e8aceb42ed1a364a92ba88498198ba5631d
thisisalvaro <alvaroblanco496@gmail.com>
2025-02-05 17:10:53 +0100

    up

--- app/reservations/calendarService.php
+++ app/reservations/calendarService.php
@@ -3,7 +3,7 @@
 
 require_once 'db.php'; // Archivo de conexión a la base de datos
 
-// Obtener todos los horarios disponibles para una fecha y cancha específica
+// Obtener todos los horarios disponibgit pullles para una fecha y cancha específica
 function obtenerHorariosDisponibles($fecha, $id_pista) {
     $horarios = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00'];
     
